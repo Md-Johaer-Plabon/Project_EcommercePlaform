d8c86f8f718031956a2d133b0eef8184e2b924c6
Plabon <mdplabonda@gmail.com>
2025-01-08 01:48:41 +0600

    Add Category Tree Table

---
+++ BechaKena/BechaKenaWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BechaKenaWeb.Data;
+using BechaKenaWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BechaKena.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
