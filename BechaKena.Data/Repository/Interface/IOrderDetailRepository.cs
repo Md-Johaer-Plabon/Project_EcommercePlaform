7a0d5e674219eafbd287577f1bd5ce044587972e
plabon <mdplabonda@gmail.com>
2025-01-26 20:04:25 +0600

    Added DB Migration of Order/Modified Shopping Cart for DB Model

---
+++ BechaKena.Data/Repository/Interface/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using BechaKena.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BechaKena.Data.Repository.Interface
+{
+	public interface IOrderDetailRepository : IRepository<OrderDetail>
+	{
+		void Update(OrderDetail obj);
+	}
+}
--- BechaKena.Data/Repository/RepositoryManager.cs
+++ BechaKena.Data/Repository/RepositoryManager.cs
@@ -19,6 +19,8 @@ namespace BechaKena.Data.Repository
 		public IShoppingCartRepository ShoppingCart { get; private set; }
 
 		public IApplicationUserRepository ApplicationUser { get; private set; }
+		public IOrderHeaderRepository OrderHeader { get; private set; }
+		public IOrderDetailRepository OrderDetail { get; private set; }
 
 		public RepositoryManager(ApplicationDbContext db)
         {
@@ -29,6 +31,8 @@ namespace BechaKena.Data.Repository
 			Company = new CompanyRepository(_db);
 			ApplicationUser = new ApplicationUserRepository(_db);
 			ShoppingCart = new ShoppingCartRepository(_db);
+			OrderHeader = new OrderHeaderRepository(_db);
+			OrderDetail = new OrderDetailRepository(_db);
 		}
         public void Save()
         {
