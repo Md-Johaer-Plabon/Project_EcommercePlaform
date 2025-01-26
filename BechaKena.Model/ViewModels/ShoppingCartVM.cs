f2f072ce6b8dcb607522ddf388f7361ecf139c4d
plabon <mdplabonda@gmail.com>
2025-01-26 20:12:59 +0600

    Loaded Summary with user data

--- BechaKena.Model/ViewModels/ShoppingCartVM.cs
+++ BechaKena.Model/ViewModels/ShoppingCartVM.cs
@@ -10,6 +10,6 @@ namespace BechaKena.Model.ViewModels
 	public class ShoppingCartVM
 	{
 		public IEnumerable<ShoppingCart> ListCart { get; set; }
-		public double CartTotal { get; set; }
+		public OrderHeader OrderHeader { get; set; }
 	}
 }
