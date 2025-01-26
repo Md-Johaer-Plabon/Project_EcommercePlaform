bca3cee170074036976f04b81b72ead4a6474b84
plabon <mdplabonda@gmail.com>
2025-01-26 17:04:58 +0600

    Modified Register View

--- BechaKena/BechaKenaWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BechaKena/BechaKenaWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using BechaKena.Model.Models;
 using BechaKena.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -99,7 +100,15 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-        }
+
+            [Required]
+            public string Name { get; set; } = string.Empty;
+			public string? StreetAddress { get; set; }
+			public string? City { get; set; }
+			public string? State { get; set; }
+			public string? PostalCode { get; set; }
+			public string? PhoneNumber { get; set; }
+		}
 
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -126,7 +135,15 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
+
+				user.StreetAddress = Input.StreetAddress;
+				user.City = Input.City;
+				user.State = Input.State;
+				user.PostalCode = Input.PostalCode;
+				user.Name = Input.Name;
+				user.PhoneNumber = Input.PhoneNumber;
+
+				var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
@@ -164,11 +181,11 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
