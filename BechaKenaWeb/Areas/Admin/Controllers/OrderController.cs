9cae8d2c1db803deda9d156fdba99eec3952703c
Plabon <mdplabonda@gmail.com>
2025-01-30 01:22:33 +0600

    Added implementation of OrderCOntroller view

--- BechaKenaWeb/Areas/Admin/Controllers/OrderController.cs
+++ BechaKenaWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using BechaKena.Data.Repository.Interface;
 using BechaKena.Model.Models;
+using BechaKena.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BechaKena.Areas.Admin.Controllers
@@ -18,11 +19,30 @@ namespace BechaKena.Areas.Admin.Controllers
 		}
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
 			IEnumerable<OrderHeader> orderHeaders;
 			orderHeaders = _db.OrderHeader.GetAll(includeProperties: "ApplicationUser");
-			return Json(new { data = orderHeaders });
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SharedDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SharedDetails.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SharedDetails.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SharedDetails.PaymentStatusDelayedPayment);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
 		}
 		#endregion
 	}
